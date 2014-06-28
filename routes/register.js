00ae999b3da8bc3e1f378d5902e61a1e47650108
shen2 <zhenyupku@gmail.com>
2014-06-29 05:19:57 +0800

    bug fix

--- routes/register.js
+++ routes/register.js
@@ -32,9 +32,9 @@ module.exports = function(deps) {
           message : '没有检测到照片中的人脸'
         });
       }
-      
+      var new_face_id = body.face[0].face_id;
       fppClient.post('recognition/identify',
-          {group_id : '0bf546544b9dee83bb5706550ada4f59', key_face_id : body.face[0].face_id},
+          {group_id : '0bf546544b9dee83bb5706550ada4f59', key_face_id : new_face_id},
           function(err, response, body){
             if (err) return next(err);
             if (body.error) return next(new Error(body.error));
@@ -63,52 +63,53 @@ module.exports = function(deps) {
               fppClient.post('person/create', {}, createUser);
             }
           });
-    });
-    function createUser(err, response, body) {
-      if (err) return next(err);
-      if (body.error) return next(new Error(body.error));
-
-      userCtrler.create({
-        _id : body.person_id,
-        nickname : '随机用户名',
-        avatar : avatarFile.name
-      }, function(err, user) {
+      
+      function createUser(err, response, body) {
         if (err) return next(err);
-        
-        fppClient.post('group/add_person', {
-          group_id : '0bf546544b9dee83bb5706550ada4f59',
-          person_id : body.person_id
-        }, function (err, response, body){
-          if (err) return next(err);
-        });
-        
-        req.session.user = user;
+        if (body.error) return next(new Error(body.error));
 
-        //user.face_id = body.face[0].face_id;
-        //user.save();
-        
-        fppClient.post('person/add_face', {
-          person_id: user._id,
-          face_id: body.face[0].face_id
-        }, function (err, response, body) {
-          // ignore err
-          if (err) return;
+        userCtrler.create({
+          _id : body.person_id,
+          nickname : '随机用户名',
+          avatar : avatarFile.name
+        }, function(err, user) {
+          if (err) return next(err);
           
-          fppClient.post('train/verify', {
-            person_id : user._id
-          }, function (err, response, body){});
-          
-          fppClient.post('train/identify', {
+          fppClient.post('group/add_person', {
             group_id : '0bf546544b9dee83bb5706550ada4f59',
-          }, function(err, response, body){});
+            person_id : body.person_id
+          }, function (err, response, body){
+            if (err) return next(err);
+          });
+          
+          req.session.user = user;
+
+          //user.face_id = body.face[0].face_id;
+          //user.save();
+          
+          fppClient.post('person/add_face', {
+            person_id: user._id,
+            face_id: new_face_id
+          }, function (err, response, body) {
+            // ignore err
+            if (err) return;
+            
+            fppClient.post('train/verify', {
+              person_id : user._id
+            }, function (err, response, body){});
+            
+            fppClient.post('train/identify', {
+              group_id : '0bf546544b9dee83bb5706550ada4f59',
+            }, function(err, response, body){});
+          });
+          
+          res.json({
+            status: 'ok',
+            user: user
+          });
         });
-        
-        res.json({
-          status: 'ok',
-          user: user
-        });
-      });
-    } //  end of createUser()
+      } //  end of createUser()
+    });
   });
 
   return route;
