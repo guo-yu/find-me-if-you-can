17a509f9e0e8db4d084ce5b5018d4f9256a2bf66
shen2 <zhenyupku@gmail.com>
2014-06-28 22:11:26 +0800

    routes/task.js

---
+++ routes/task.js
@@ -0,0 +1,38 @@
+module.exports = function(app, express, debug, ctrlers) {
+  var route = express.Router();
+
+  // upload my avatar or pictures
+  route.get('/', function(req, res, next) {
+    if (!req.session.user)
+      return next(401);
+    
+    ctrlers.photo.model.find({ persons : req.session.user._id})
+      .exec(callback1);
+    
+    function callback1(err, photos){
+      var alreadyTokenPersons = [];
+      photos.forEach(function(photo){
+        alreadyTokenPersons.push(photo.subject);
+      });
+      
+      //req.session.user.gender;
+      ctrlers.user.model.find({_id : {$nin : alreadyTokenPersons}}, callback2);
+    }
+    
+    var response = {};
+    function callback2(err, users){
+      response.user = users[Math.floor(Math.random() * users.length)];
+      
+      ctrlers.photo.model.find({subject : response.user._id})
+        .exec(callback3);
+    }
+    
+    function callback3(err, photos){
+      response.photos = photos;
+      
+      res.json(response);
+    }
+  });
+
+  return route;
+};
